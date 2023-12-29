b2163e39118ca78016225b615d9f055f84be82bd
ltrentin <luckytrentin@gmail.com>
2023-12-29 19:02:17 -0300

    sarasa

---
+++ main.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace main
+{
+    {
+        internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
+}
